e70a417e9f9df41030fe08197c854cf87d117438
vaishubhsale785-hash <vaishubhsale785@gmail.com>
2026-05-13 22:58:08 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,7 +1,5 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, {
-  polling: false, // IMPORTANT
-});
+const bot = new TelegramBot(process.env.BOT_TOKEN);
 
 module.exports = bot;
